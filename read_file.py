fd233768d0d91d07579a9a7ebd738a2559b6e14b
AnshuOnGit <anshu.kumar726@gmail.com>
2016-01-26 20:53:53 +0530

    Added path exists validation

--- read_file.py
+++ read_file.py
@@ -1,6 +1,7 @@
 __author__ = 'anshu'
 
 from flask import Flask
+import os.path as opath
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -14,6 +15,8 @@ def spit_file_on_browser(file_name):
     return read_file(file_name)
 
 def read_file(file_path):
+    if not opath.exists(file_path):
+        return 'Sorry you have wrong file.'
     with open(file_path,'r') as file:
         return file.read()
 
